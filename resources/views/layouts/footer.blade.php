b66edf607f765e345c9794f42b69bc35de8b8975
Anna Lorraine Caandoy <82627857+caandoyAnna@users.noreply.github.com>
2021-05-07 16:43:48 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -8,8 +8,65 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <footer class="py-3">
-        <div class="container"><div class="small text-center text-muted">Copyright © 2021 - QuaraNtimes</div></div>
+    <footer id="footer">
+        <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+            <div class="col-lg-3 col-md-6">
+                <div class="footer-info">
+                <h3>QuaraNtimes</h3>
+                <p>
+                12-B Alcalde Jose<br>
+                    Pasig City, PH.<br><br>
+                    <strong>Phone:</strong> +2 (134) 190-4017<br>
+                    <strong>Email:</strong> quaraNtimes_Help@gmail.com<br>
+                </p>
+                <div class="social-links mt-3">
+                    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                </div>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-6 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Related to Us</h4>
+                <ul>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Worldometer.info</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">QuaraNtimes Mobile</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-4 col-md-6 footer-newsletter">
+                <h4>Our Newsletter</h4>
+                <p>Have us subscribe on our newsletter!</p>
+                <form action="" method="post">
+                <input type="email" name="email"><input type="submit" value="Subscribe">
+                </form>
+
+            </div>
+
+            </div>
+        </div>
+        </div>
+
+        <div class="container">
+            <div class="copyright">Copyright © 2021 - QuaraNtimes</div>
+        </div>
     </footer>
 </body>
 </html>
