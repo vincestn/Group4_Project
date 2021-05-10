e2a8f553b3e00dc7547fe581ae3ed5f240631857
vincestn <johnvincentsta.ana123456789@gmail.com>
2021-05-10 10:45:42 +0800

    Update on Admin Page and CSS
    -Update on adminMain.blade.php
    -New file, meta.blade.php
    -New file, adminArticles.blade.php for initial static articles
    -CSS for symptoms(mainpage) and steps(adminpage)

---
+++ resources/views/adminArticles.blade.php
@@ -0,0 +1,51 @@
+    <!-- ======= Steps Section ======= -->
+    <section id="steps" class="steps section-bg">
+      <div class="container">
+        <div class="row no-gutters">
+
+          <div class="col-lg-4 col-md-6 content-item" data-aos="fade-in">
+            <span>01</span>
+            <h4>Philippines' COVID-19 deaths breach 14,000 as country logs 6,414 new cases</h4>
+            <h6>The Philippines now has 819,164 cases of COVID-19 </h6>
+            <p>Posted by Calayo, on April 08, 2021</p>
+            <br>
+            &emsp;<a href="{{ url('/create') }}" class="btn-edit scrollto">Edit Article</a> &emsp;
+            <a href="{{ url('/create') }}" class="btn-delete scrollto">Delete Article</a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item" data-aos="fade-in">
+            <span>02</span>
+            <h4>Philippines man dies after being forced
+                to do 300 squats for breaking Covid-19 curfew</h4>
+            <h6>A tough approach to Covid restrictions </h6>
+            <p>Posted by Calayo, on April 08, 2021</p>
+            <br>
+            &emsp;<a href="{{ url('/create') }}" class="btn-edit scrollto">Edit Article</a> &emsp;
+            <a href="{{ url('/create') }}" class="btn-delete scrollto">Delete Article</a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item" data-aos="fade-in">
+            <span>03</span>
+            <h4>Cambodia among first countries to receive
+                COVID-19 vaccines from COVAX Facility</h4>
+            <h6>Joint News Release Phnom Penh International Airport </h6>
+            <p>Posted by Caandoy, on April 10, 2021</p>
+            <br>
+            &emsp;<a href="{{ url('/create') }}" class="btn-edit scrollto">Edit Article</a> &emsp;
+            <a href="{{ url('/create') }}" class="btn-delete scrollto">Delete Article</a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item" data-aos="fade-in">
+            <span>04</span>
+            <h4>Lao PDR Prepares for Roll-out of COVID-19 VaccinesP</h4>
+            <h6>Vaccines will be provided first to those at highest risk of getting the disease </h6>
+            <p>Posted by Caandoy, on April 10, 2021</p>
+            <br>
+            &emsp;<a href="{{ url('/create') }}" class="btn-edit scrollto">Edit Article</a> &emsp;
+            <a href="{{ url('/create') }}" class="btn-delete scrollto">Delete Article</a>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Steps Section -->
--- resources/views/layouts/adminMain.blade.php
+++ resources/views/layouts/adminMain.blade.php
@@ -1,218 +1,118 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-   	  <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-   <!-- Styles -->
-   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-   
-   <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Home</title>
+        <title>Admin</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="img/logo.png" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
-        <!-- Third party plugin CSS-->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
+        <!--meta-->
+        @include('layouts.meta')
    </head>
    
    <body> 
-        <div class="d-flex" id="wrapper" >
-
-        <!-- Sidebar -->
-        <div style="width:300px;">
-            <div class="bg-light border-right" id="sidebar-wrapper">
-                <br>
-                <div class="sidebar-heading text-center">QuaraNtimes</div>
-                <div class="list-group list-group-flush mt-2">
-                    <a href="https://www.worldometers.info/coronavirus/" class="list-group-item list-group-item-action bg-light text-center">Live Update</a>
-                    <a href="#LocalNews" class="list-group-item list-group-item-action bg-light text-center">Local News</a>
-                    <a href="#InterNews" class="list-group-item list-group-item-action bg-light text-center">International News</a>
-                    <a href="#Symptoms" class="list-group-item list-group-item-action bg-light text-center">COVID-19 Symptoms</a>
-                    <a href="{{ url('/create') }}" class="list-group-item list-group-item-action bg-light text-center">Publish Article</a>
-                    <a href="{{ url('/index#mobile ') }}" class="list-group-item list-group-item-action bg-light text-center">QuaraNtimes Mobile</a>
-                    <a href="{{ url('/index') }}" class="list-group-item list-group-item-action bg-light text-center">Overview Page</a>
+        <!-- Navbar Admin -->
+        <header id="header" class="fixed-top py-2">
+            <div class="container d-flex">
+                <div class="logo mr-auto">
+                    <h1 class="text-light"><a href="{{ url('/') }}">QuaraNtimes</a></h1>
                 </div>
-            </div>
-        </div>
-        <!-- /#sidebar-wrapper -->
+                <nav class="navbar navbar-expand-lg nav-menu d-none d-lg-block">
+                    <div class="container">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-        <!-- Page Content -->
-        <div class="container border-left" id="page-content-wrapper">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!-- Left Side Of Navbar -->
+                            <ul class="navbar-nav mr-auto"></ul>
 
-            <!-- Navbar Mainpage -->
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top px-5">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'QuaraNtimes') }}
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+                            <!-- Right Side Of Navbar -->
+                            <ul class="navbar-nav ml-auto">
+                                <!-- Authentication Links -->
+                                    @guest
+                                        @if (Route::has('login'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('login') }}">{{ __('LOGIN') }}</a>
+                                            </li>
+                                        @endif
+                                        
+                                        @if (Route::has('register'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('register') }}">{{ __('REGISTER') }}</a>
+                                            </li>
+                                        @endif
+                                    @else
+                                        <li class="nav-item dropdown">
+                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                {{ Auth::user()->name }}
+                                            </a>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
+                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                                <a class="dropdown-item text-dark" href="{{ route('logout') }}"
+                                                onclick="event.preventDefault();
+                                                                document.getElementById('logout-form').submit();">
+                                                    {{ __('Logout') }}
+                                                </a>
 
-                        </ul>
-
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('LOGIN') }}</a>
-                                    </li>
-                                @endif
-                                
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('REGISTER') }}</a>
-                                    </li>
-                                @endif
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }}
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-
-            <!-- Live Update -->
-            <header class="masthead" id="LiveUpdate">
-                @yield('cover')
-                    <div class="row h-100 align-items-center justify-content-center text-center">
-                        <div class="col-lg-10 align-self-end">
-                            <br><br>
-                            <h3 class="display-4 text-uppercase text-white font-weight-bold">@yield('coverTitle')</h1>
-                            <h3 class="text-danger font-weight-bold">@yield('subTitle')</h1>
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                    @csrf
+                                                </form>
+                                            </div>
+                                        </li>
+                                    @endguest
+                                </ul>
+                            </div>
                         </div>
-                        <div class="col-lg-8 align-self-baseline">
-                        <br>
-                            <p class="text-white font-italic">Last updated: April 07, 2021, 02:25 GMT. Visit Worldometer to view detailed reports.</p>
-                            <a class="btn btn-secondary btn-lg" href="https://www.worldometers.info/coronavirus/" role="button">Visit Now</a>
-                        </div>
-                    </div>
+                    </nav>
                 </div>
             </header>
+            <!-- End of Navbar Admin-->
 
-                
-            <!-- Main Content-->
-            <br><br>
+            <!-- Add Articles -->
+            <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+                <div class="col-lg-8 col-md-10 mx-auto text-center" data-aos="fade-up">
+                    <h3 class="display-4 text-uppercase text-white font-weight-bold">Publish Articles</h1>
+                    <h2><br>
+                    <p class="lead">Publish new articles anything related to the Covid-19 Pandemic.</p>
+                    <hr class="my-4 ">
+                    <a href="{{ url('/create') }}" class="btn-get-started scrollto">Create Article</a>
+                </div>
+            </section>
+
+            
+            <!--Live Update -->
 
             <!-- Local News -->
-            <div class="card text-white my-1 py-1 text-center" style="background-image: url(img/news.jpg); 
+            <div class="card text-white py-1 text-center" style="background-image: url(img/news.jpg); 
                 height: 130px; background-repeat: no-repeat; background-size:cover;" id="LocalNews">
-                <div class="card-body">
-                    <h1 class="section-heading">Local News</h2>
-                    <p class="text-white m-0">Daily news from the Philippines to notify the citizens.</p>
+                <div class="card-body" data-aos="fade-up">
+                    <h1 class="section-heading">List Of Articles</h2>
+                    <p class="text-white m-0">Update or delete certain articles</p>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-lg-8 col-md-10 mx-auto">
-                    <div class="post-preview">
-                        @yield('postL1')
-                    </div>
-                    <hr />   
-                </div>
-            </div>
+        @include('adminArticles')
 
-            <div class="row">
-                <div class="col-lg-8 col-md-10 mx-auto">
-                    <div class="post-preview">
-                        @yield('postL2')
-                    </div>
-                    <hr />   
-                </div>
-            </div>
-                    
-            <hr />
+        @include('layouts.footer')
+        <!-- Vendor JS Files -->
+        <script src="{{ asset('/vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+        <script src="{{ asset('/vendor/php-email-form/validate.js') }}"></script>
+        <script src="{{ asset('/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+        <script src="{{ asset('/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+        <script src="{{ asset('/vendor/venobox/venobox.min.js') }}"></script>
+        <script src="{{ asset('/vendor/aos/aos.js') }}"></script>
 
-            <!-- International News -->
-            <div class="card text-white my-1 py-1 text-center" style="background-image: url(img/news.jpg); 
-                height: 130px; background-repeat: no-repeat; background-size:cover;" id="InterNews">
-                <div class="card-body">
-                    <h1 class="section-heading">International News</h2>
-                    <p class="text-white m-0">News to inform you what happens around the globe.</p>
-                </div>
-            </div>
+        <!-- Template Main JS File -->
+        <script src="{{ asset('/js/main.js') }}"></script>
 
-            <div class="row">
-                <div class="col-lg-8 col-md-10 mx-auto">
-                    <div class="post-preview">
-                        @yield('postI1')
-                    </div>
-                    <hr />   
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-8 col-md-10 mx-auto">
-                    <div class="post-preview">
-                        @yield('postI2')
-                    </div>
-                    <hr />   
-                </div>
-            </div>
-
-            </div>
-            <hr /> 
-            <!-- /#page-content-wrapper -->
-
-        </div>
-        
-        <br>
-          <!-- Symptoms -->
-          <div class="container-xxl" id="Symptoms">
-                <div class="card text-white my-1 py-1 text-center" style="background-image: url(img/symp.jpg); 
-                    height: 325px; background-repeat: no-repeat; background-size:cover;" id="Symptoms">
-                    <div class="card-body">
-                        <br>
-                        <h1 class="section-heading text-white">COVID-19 Symptoms</h2>
-                        <p class="text-white m-0">Be aware, you might have it without knowing.</p>
-                        <p class="text-white m-0">Article by Feca, John Neil G.</p>
-                        <br><br>
-                        <a class="btn btn-secondary btn-lg" href="{{ url('/postSymp') }}" role="button">Learn More</a>
-                    </div>
-                        @include('layouts.footer')
-                </div>  
-            </div>
-
-        <!-- /#wrapper -->
-
-         <!-- Bootstrap core JS-->
-         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <!-- Bootstrap core JS-->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        
         <!-- Third party plugin JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+
    </body>
 </html>
