8bacd1f67a1f0ef9a4f49678e5d2971bf4f3b62c
Mark Julius Mella <82628183+awmicalayers@users.noreply.github.com>
2021-06-03 01:08:31 +0800

    Update adminMain.blade.php

--- resources/views/layouts/adminMain.blade.php
+++ resources/views/layouts/adminMain.blade.php
@@ -134,6 +134,7 @@
                 </div>
             </div>
 
+<<<<<<< Updated upstream
             <div class="row">
                 <div class="col-lg-8 col-md-10 mx-auto">
                     <div class="post-preview">
